82f123ffa82eb4be2927d4585440616dcb1dc9c4
caelstuffs <caelstuffs@gmail.com>
2021-03-10 08:53:26 -0500

    Player Setup

---
+++ Group4Church/Assets/mouselook.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class mouselook : MonoBehaviour
+{
+    public float MouseSpeed;
+    public Transform playerBody;
+
+    float xRot = 0f;
+    
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    void Update()
+    {
+        float mouseX = Input.GetAxis("Mouse X") * MouseSpeed * Time.deltaTime;
+        float mouseY = Input.GetAxis("Mouse Y") * MouseSpeed * Time.deltaTime;
+
+        xRot -= mouseY;
+        xRot = Mathf.Clamp(xRot, -90f, 90f);
+
+        transform.localRotation = Quaternion.Euler(xRot, 0f, 0f);
+        playerBody.Rotate(Vector3.up * mouseX);
+    }
+}
